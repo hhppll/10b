1324250d3c29e51fdb25e11b8a1adb27380f87fa
hhppll <1303474648@qq.com>
2019-05-06 16:26:56 +0800

    第一次

---
+++ User.java
@@ -0,0 +1,97 @@
+package com.jk.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private Integer id;
+    private String name;
+    private String password;
+    private String remPwd;
+    private String sex;
+    private Integer age;
+    private String brithday;
+    private String img;
+    private String email;
+    private String emailCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRemPwd() {
+        return remPwd;
+    }
+
+    public void setRemPwd(String remPwd) {
+        this.remPwd = remPwd;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getBrithday() {
+        return brithday;
+    }
+
+    public void setBrithday(String brithday) {
+        this.brithday = brithday;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmailCode() {
+        return emailCode;
+    }
+
+    public void setEmailCode(String emailCode) {
+        this.emailCode = emailCode;
+    }
+}
